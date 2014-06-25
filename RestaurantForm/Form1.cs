7664ccf5dcae20733314391606e01484385a7b7f
Maxim Lyukshin <lyukshin.maxim@gmail.com>
2014-06-25 14:19:54 +0400

    Moved some projects folders on disk

---
+++ RestaurantForm/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestaurantLib;
+
+namespace RestaurantForm
+{
+    public partial class Form1 : Form
+    {
+        int currentDish;
+        //BinaryRepository fileRepo;
+		IRepository fileRepo;
+
+        List<Dish> selectedDishes;
+
+        public Form1()
+        {
+         
+            InitializeComponent();
+
+            selectedDishes = new List<Dish>();
+			fileRepo = new XmlRepository("dishes.dat");//new BinaryRepository("dishes.dat");
+            fileRepo.Add(new Dish("Кукруза", 12.6m));
+            fileRepo.Add(new Dish("Картофель", 15.6m));
+            fileRepo.Add(new Dish("Морковь", 14.6m));
+            //fileRepo.OpenFile();
+            label3.Text = fileRepo.GetDishes().ElementAt(0).Name;
+            label4.Text = fileRepo.GetDishes().ElementAt(0).Price.ToString();
+            currentDish = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (currentDish != 0)
+            {
+                currentDish--;
+                label3.Text = fileRepo.GetDishes().ElementAt(currentDish).Name;
+                label4.Text = fileRepo.GetDishes().ElementAt(currentDish).Price.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (currentDish != fileRepo.GetDishes().Count() - 1)
+            {
+                currentDish++;
+                label3.Text = fileRepo.GetDishes().ElementAt(currentDish).Name;
+                label4.Text = fileRepo.GetDishes().ElementAt(currentDish).Price.ToString();
+                
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (((CheckBox)sender).CheckState == CheckState.Checked)
+            {
+                selectedDishes.Add(fileRepo.GetDishes().ElementAt(currentDish));
+            }
+            else if (((CheckBox)sender).CheckState == CheckState.Unchecked)
+            {
+                selectedDishes.Remove(fileRepo.GetDishes().ElementAt(currentDish));
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (Dish d in selectedDishes)
+            {
+                MessageBox.Show("Блюдо " + d.Name);
+            }
+        }
+    }
+}
---
+++ RestaurantLib/BinaryRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace RestaurantLib
+{
+	public class BinaryRepository : IRepository
+	{
+		private string fileName;
+        BinaryFormatter bf;
+        private List<Dish> dishes;
+
+		public BinaryRepository(string fileName)
+        {
+            this.fileName = fileName;
+            bf = new BinaryFormatter();
+            dishes = new List<Dish>();
+		}
+
+		public void OpenFile()
+		{
+            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                dishes = (List<Dish>)bf.Deserialize(fs);
+            }
+		}
+
+		public IEnumerable<Dish> GetDishes()
+		{
+		    return dishes;
+		}
+
+		public void Add(Dish dish)
+		{
+            dishes.Add(dish);
+		}
+
+		public void Delete(Dish dish)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Edit(Dish dish)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Save()
+		{
+            using (Stream fStream = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                bf.Serialize(fStream, dishes);
+            }
+		}
+	}
+
+    
+}
