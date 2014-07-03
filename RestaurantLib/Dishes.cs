211e798509fda28660c224a94c35e32f7926ef76
Maxim Lyukshin <lyukshin.maxim@gmail.com>
2014-07-03 14:32:26 +0400

    Add class Dishes (:IEnumerable)

---
+++ RestaurantLib/Dishes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantLib
+{
+	class Dishes : IEnumerable
+	{
+		private List<Dish> dishes = new List<Dish>();
+
+		public Dish this[int index]
+		{
+			get { return dishes[index]; }
+			set { dishes[index] = value; }
+		}
+		public IEnumerator GetEnumerator()
+		{
+			return dishes.GetEnumerator();
+		}
+	}
+}
