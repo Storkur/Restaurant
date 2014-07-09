8a97927c1f13081362490d4d13dac8c3ff21337a
Maxim Lyukshin <lyukshin.maxim@gmail.com>
2014-07-09 10:48:49 +0400

    Реализовано удаление, добавление и изменение блюд с формы

---
+++ RestaurantForm/DishForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace RestaurantForm
+{
+    partial class DishForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.btnOk = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textName = new System.Windows.Forms.TextBox();
+			this.numPrice = new System.Windows.Forms.NumericUpDown();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.btnApply = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.numPrice)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btnOk
+			// 
+			this.btnOk.Location = new System.Drawing.Point(15, 181);
+			this.btnOk.Name = "btnOk";
+			this.btnOk.Size = new System.Drawing.Size(72, 23);
+			this.btnOk.TabIndex = 0;
+			this.btnOk.Text = "OK";
+			this.btnOk.UseVisualStyleBackColor = true;
+			this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(205, 181);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(72, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Отмена";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// textName
+			// 
+			this.textName.Location = new System.Drawing.Point(123, 30);
+			this.textName.Name = "textName";
+			this.textName.Size = new System.Drawing.Size(138, 20);
+			this.textName.TabIndex = 2;
+			// 
+			// numPrice
+			// 
+			this.numPrice.DecimalPlaces = 2;
+			this.numPrice.Location = new System.Drawing.Point(123, 70);
+			this.numPrice.Name = "numPrice";
+			this.numPrice.Size = new System.Drawing.Size(138, 20);
+			this.numPrice.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(57, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Название";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 77);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(33, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Цена";
+			// 
+			// btnApply
+			// 
+			this.btnApply.Location = new System.Drawing.Point(110, 181);
+			this.btnApply.Name = "btnApply";
+			this.btnApply.Size = new System.Drawing.Size(72, 23);
+			this.btnApply.TabIndex = 6;
+			this.btnApply.Text = "Применить";
+			this.btnApply.UseVisualStyleBackColor = true;
+			this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+			// 
+			// DishForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.btnApply);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.numPrice);
+			this.Controls.Add(this.textName);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.btnOk);
+			this.Name = "DishForm";
+			this.Text = "DishForm";
+			((System.ComponentModel.ISupportInitialize)(this.numPrice)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.NumericUpDown numPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button btnApply;
+    }
+}
---
+++ RestaurantForm/DishForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestaurantLib;
+
+namespace RestaurantForm
+{
+	public enum Modes
+	{
+		Add,
+		Edit,
+		Remove
+	};
+    public partial class DishForm : Form
+    {
+        MainForm parent;
+        Dish dish;
+		Modes mode;
+        public DishForm(MainForm parent, Modes mode, Dish dish = null)
+        {
+            InitializeComponent();
+
+            this.parent = parent;
+			this.mode = mode;
+            if (dish != null)
+            {
+                this.dish = dish;
+				textName.Text = dish.Name;
+				numPrice.Value = dish.Price;
+			}
+		}
+
+		private void btnOk_Click(object sender, EventArgs e)
+		{
+			Apply();
+			this.Close();
+		}
+
+		private void Apply()
+		{
+			if (mode == Modes.Edit)
+			{
+				parent.EditDish(new Dish(dish.Name, numPrice.Value));
+			}
+			else if (mode == Modes.Add)
+			{
+				parent.AddDish(new Dish(textName.Text, numPrice.Value));
+			}
+			else if (mode == Modes.Remove)
+			{
+				parent.RemoveDish(new Dish(textName.Text, numPrice.Value));
+			}
+		}
+
+		private void btnApply_Click(object sender, EventArgs e)
+		{
+
+		}
+
+
+	}
+}
